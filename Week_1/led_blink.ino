7e5550da877d2ca2a24b36b661dc999f8f77b702
Delta_69 <chydibya2006@gmail.com>
2026-07-05 21:43:37 +0530

    v1.3
    Added a potentiometer to control blink speed

--- Week_1/led_blink.ino
+++ Week_1/led_blink.ino
@@ -1,13 +1,19 @@
-int blink_count = 1;
+const int POT_PIN = A0;
+const int LED_PIN = 2;
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  pinMode(POT_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
+
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
-  Serial.print("Blink Count:");
-  Serial.println(blink_count++);
+  // put your main code here, to run repeatedly:
+  int POT_VALUE = analogRead(POT_PIN);
+  Serial.println(POT_VALUE);
+  float time = (5000.0/1024.0)*POT_VALUE;
+  digitalWrite(LED_PIN, HIGH);
+  delay(time);
+  digitalWrite(LED_PIN, LOW);
+  delay(time);
 }
