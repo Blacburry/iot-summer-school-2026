ef61df42dc3f3524f0eaae65bb2ec68d71438c57
Delta_69 <chydibya2006@gmail.com>
2026-07-06 03:12:36 +0530

    fix: Added RGB LED support Fixes #3
    The LED will change it's colour from green to red as the blinking slows down.

--- Week_1/led_blink.ino
+++ Week_1/led_blink.ino
@@ -16,37 +16,41 @@
  */
 
 // Global variables (from your previous versions)
-int blinkCount = 0;
-const int POT_PIN = A0;
-const int LED_PIN = 13;
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  Serial.begin(9600);
+const int POT_PIN = A0;
+const int LED_PIN[] = {2, 3, 4};
+int totalPins = sizeof(LED_PIN)/sizeof(LED_PIN[0]);
+
+void ledBlink(int pin, float t) {
+  digitalWrite(pin, HIGH);
+  delay(t);
+  digitalWrite(pin, LOW);
+  delay(t);
 }
 
-void loop() {
-  // Your Q4 v1.2 logic reading the potentiometer and blinking goes here...
-}
-const int POT_PIN = A0;
-const int LED_PIN = 2;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(POT_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
+  for (int i=0; i<totalPins; i++) {
+    pinMode(LED_PIN[i], OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int POT_VALUE = analogRead(POT_PIN);
   Serial.println(POT_VALUE);
-  if (POT_VALUE == 0) {
-    POT_VALUE = 1;
-  }
+  if (POT_VALUE == 0) POT_VALUE = 1;
   float time = (5000.0/1024.0)*POT_VALUE;
-  digitalWrite(LED_PIN, HIGH);
-  delay(time);
-  digitalWrite(LED_PIN, LOW);
-  delay(time);
+  if (POT_VALUE > 0 && POT_VALUE <= 341) {
+    ledBlink(LED_PIN[0], time);
+  }
+  else if (POT_VALUE <= 682) {
+    ledBlink(LED_PIN[1], time);
+  }
+  else {
+    ledBlink(LED_PIN[2], time);
+  }
 }
+
